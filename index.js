0856bf03ed2f43502e5b91bac81e0cdaa41d367a
Danger08 <danielgabriel08@hotmail.com>
2016-07-05 16:53:37 +0800

    try

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-users = [];
-connection = [];
-app.get('/', function(req, res){
-  res.sendFile('index.html');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-io.on('connection', function(socket){
-  io.emit('chat message' , 'a user conected');
-
-
-  socket.on('disconnect', function(){
-  console.log('user disconnected');
-  });
-
-  socket.on('chat message' , function(msg){
-    io.emit('chat message' , msg);
-  });
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:5000');
-});
+
